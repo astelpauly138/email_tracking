93656b4ce67406d50920c4102dfb771411fb3e4a
astelpauly138 <a.pauly@analytica-data.com>
2026-02-20 15:12:53 +0400

    added link clicking endpoint

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import Response
+from fastapi import FastAPI
+from fastapi.responses import Response, RedirectResponse
 from supabase_client import supabase
 import base64
 
 app = FastAPI()
 
+
+# -----------------------
+# OPEN TRACKING
+# -----------------------
 @app.get("/track")
 async def track_email(
     u: str,
@@ -21,6 +25,7 @@ async def track_email(
         .eq("lead_id", l) \
         .eq("user_id", u) \
         .eq("campaign_id", c) \
+        .eq("event_type", "sent") \
         .execute()
 
     print("Supabase response:", response.data)
@@ -31,3 +36,33 @@ async def track_email(
     )
 
     return Response(content=pixel, media_type="image/gif")
+
+
+# -----------------------
+# CLICK TRACKING
+# -----------------------
+@app.get("/click")
+async def track_click(
+    u: str,
+    c: str,
+    l: str,
+    redirect: str
+):
+
+    print("CLICKED:")
+    print("User:", u)
+    print("Campaign:", c)
+    print("Lead:", l)
+
+    response = supabase.table("email_events") \
+        .update({"event_type": "clicked"}) \
+        .eq("lead_id", l) \
+        .eq("user_id", u) \
+        .eq("campaign_id", c) \
+        .eq("event_type", "opened") \
+        .execute()
+
+    print("Supabase click response:", response.data)
+
+    # Redirect to actual website
+    return RedirectResponse(url=redirect)
